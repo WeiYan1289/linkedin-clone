3607baadad3bac601dff9c91ea9dabb26cc8f561
WeiYan <weiyan1289@gmail.com>
2022-07-02 22:15:12 +0800

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build a LinkedIn CLONE</h1>
 
       {/* Header */}
+      <Header />
+
       {/* App Body */}
         {/* Sidebar */}
         {/* Feed */}
---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import HeaderOption from './HeaderOption';
+
+function Header() {
+  return (
+    <div className="header">
+
+        <div className="header__left">
+            <img src={process.env.PUBLIC_URL + '/linkedin.png'}  alt="" />
+
+            <div className="header__search">
+                <SearchIcon />
+                <input type="text" />
+            </div>
+        </div>
+
+        <div className="header__right">
+            <HeaderOption Icon={HomeIcon} title="Home" />
+            <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+            <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+            <HeaderOption Icon={ChatIcon} title="Messaging" />
+            <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+            <HeaderOption avatar={process.env.PUBLIC_URL + '/logo192.png'} title="me" />
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
