6b40a0f98090078dc4e045bf0106868d0884ca8a
WeiYan <weiyan1289@gmail.com>
2022-07-11 12:38:14 +0800

    Change Secret Key for Firebase Configs

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from '@firebase/firestore'
+import { getFirestore } from '@firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC9Fi-TpR-qBCFgIM5jX7rZW4Jj3r96Mf0",
-    authDomain: "linkedin-clone-45d6a.firebaseapp.com",
-    projectId: "linkedin-clone-45d6a",
-    storageBucket: "linkedin-clone-45d6a.appspot.com",
-    messagingSenderId: "11782882673",
-    appId: "1:11782882673:web:9f254d13b9ab77d9ca22d0"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_PROJECT_APP_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
